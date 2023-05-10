060313865782673615d604a898c770fe884e9e5a
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-10 13:04:57 +0530

    Add test and behaviour for increment score

--- src/test/PlayerTest.java
+++ src/test/PlayerTest.java
@@ -9,10 +9,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
-
     @Nested
     class TestConstructor {
-
         @Test
         public void expectPlayerToBeCreated(){
             assertDoesNotThrow(() -> new Player("A", new CopyCat()));
@@ -22,8 +20,18 @@ class PlayerTest {
         public void expectExceptionForEmptyName(){
             assertThrows(IllegalArgumentException.class, () -> new Player("", new CopyCat()));
         }
+    }
 
-
+    @Nested
+    class TestIncrementScore {
+        @Test
+        public void expectScoreToIncreaseAccordingToThePoints(){
+            Player aman = new Player("A", new CopyCat());
+            Integer prevScore = aman.score;
+            aman.incrementScore(4);
+            assertEquals(aman.score, 4);
+            assertNotEquals(aman.score, prevScore);
+        }
     }
 
 }
